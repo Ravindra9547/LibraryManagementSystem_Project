6b582520267a8f81b27bb6c1f9e112887caabd46
Ravindra <Ravindra@LAPTOP-P8QNFGB6.mshome.net>
2023-03-03 13:00:57 +0530

    commit

---
+++ librarymanagementsystemproject/src/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,10 @@
+package librarymanagementsystem;
+
+//this is main entry point
+public class LibraryManagementSystem {
+public static void main(String []args)
+{
+	System.out.println("\t~~Welcome To Anudip E-Library~~");
+	AdminAndLibrarian.AllOPerations();
+}
+}
